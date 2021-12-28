c9cd1245b5d89da54035404ba33e1c787a2c1a20
MAC <miguelangelcabrera1@hotmail.com>
2021-12-28 17:45:29 +0100

    Add Tokens view for api

--- citibeats_challenge/api/views.py
+++ citibeats_challenge/api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import status, views
+from rest_framework.response import Response
 
-# Create your views here.
+from api.serializers import TokenSerializer, TextSerializer
+from core import tokenize_text
+
+
+class TokensView(views.APIView):
+    """
+    API endpoint that allows request for tokenize a text.
+    """
+    name = "tokens-viewset"
+
+    def post(self, request):
+        text_lang = TextSerializer(data=request.data)
+        text_lang.is_valid(raise_exception=True)
+        tokens = tokenize_text.get_tokens(**text_lang.data)
+        tokens = TokenSerializer(tokens).data
+        return Response(tokens, status=status.HTTP_200_OK)
