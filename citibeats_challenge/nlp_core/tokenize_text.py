9a3e6ebae1f1ce5ae2b3d0623940f53c557cbbb4
MAC <miguelangelcabrera1@hotmail.com>
2021-12-31 15:48:14 +0100

    Remove unused code

--- citibeats_challenge/nlp_core/tokenize_text.py
+++ citibeats_challenge/nlp_core/tokenize_text.py
@@ -11,35 +11,6 @@ from spacy.lang import en
 
 from common.lang import LanguageISO
 
-TOKEN_NORMALIZED_CASES_EN = (
-    NOUN,
-    ADV,
-    AUX,
-    ADJ,
-    PRON,
-)
-
-TOKEN_LEMMA_CASES_FR = (
-    PRON,
-    ADP,
-    DET,
-)
-
-TOKEN_NORMALIZED_CASES_FR = (
-    AUX,
-)
-
-TOKEN_NORMALIZED_CASES_PT = (
-    ADV,
-    AUX,
-    ADJ,
-    PRON,
-)
-
-TOKEN_LEMMA_CASES_PT = (
-    ADV,
-)
-
 def _get_core_web_sm_by_lang(lang: Optional[str] = "en") -> str:
 
     if lang == "en":
@@ -53,93 +24,7 @@ def _get_core_web_sm_by_lang(lang: Optional[str] = "en") -> str:
     elif lang == "ca":
         return "ca_core_news_sm"
 
-def _perform_tokenization_rules_for_en(doc) -> List[str]:
-    """Perform tokenization rules for English, this should be the base for other languages
-    when Spacy refines its tokenization algorithm for other languages."""
-    result_tokens = []
-    for token in doc:
-        print(f"text: {token.text}, pos: {token.pos_}, dep: {token.dep_}, norm: {token.norm_}, lemma: {token.lemma_}, morph: {token.morph}")
-        if not token.is_punct and not token.is_quote and token.pos != SYM:
-            result_tokens.append(
-                token.norm_
-                if token.pos in TOKEN_NORMALIZED_CASES_EN
-                else token.text
-            )
-    return result_tokens
 
-
-def _perform_tokenization_rules_for_es(doc) -> List[str]:
-
-    result_tokens = []
-    for token in doc:
-        print(f"text: {token.text}, pos: {token.pos_}, dep: {token.dep_}, norm: {token.norm_}, lemma: {token.lemma_}, morph: {token.morph}")
-        if not token.is_punct and not token.is_quote and token.pos != SYM:
-            result_tokens.append(
-                token.norm_
-                if token.pos in TOKEN_NORMALIZED_CASES_EN
-                else token.text
-            )
-    return result_tokens
-
-
-def _perform_tokenization_rules_for_fr(doc) -> List[str]:
-    """Perform specific rules for French due for pronouns Spacy's treatment 
-    is different than the English one."""
-
-    result_tokens = []
-    for token in doc:
-        print(f"text: {token.text}, pos: {token.pos_}, dep: {token.dep_}, norm: {token.norm_}, lemma: {token.lemma_}, morph: {token.morph}")
-        if not token.is_punct and not token.is_quote and token.pos != SYM:
-            if token.pos in TOKEN_LEMMA_CASES_FR:
-                result_tokens.append(token.lemma_)
-            elif token.pos in TOKEN_NORMALIZED_CASES_FR:
-                result_tokens.append(token.norm_)
-            else: result_tokens.append(token.text)
-    return result_tokens
-
-
-def _perform_tokenization_rules_for_pt(doc) -> List[str]:
-    result_tokens = []
-    for token in doc:
-        print(f"text: {token.text}, pos: {token.pos_}, dep: {token.dep_}, norm: {token.norm_}, lemma: {token.lemma_}, morph: {token.morph}")
-        if not token.is_punct and not token.is_quote and token.pos != SYM:
-            if token.pos in TOKEN_LEMMA_CASES_PT:
-                result_tokens.append(token.lemma_)
-            elif token.pos in TOKEN_NORMALIZED_CASES_PT:
-                result_tokens.append(token.norm_)
-            else: result_tokens.append(token.text)
-    return result_tokens
-
-def _perform_tokenization_rules_for_ca(doc) -> List[str]:
-
-    result_tokens = []
-    for token in doc:
-        print(f"text: {token.text}, pos: {token.pos_}, dep: {token.dep_}, norm: {token.norm_}, lemma: {token.lemma_}, morph: {token.morph}")
-        if not token.is_punct and not token.is_quote and token.pos != SYM:
-            result_tokens.append(
-                token.norm_
-                if token.pos in TOKEN_NORMALIZED_CASES_EN
-                else token.text
-            )
-    return result_tokens
-
-def _perform_tokenization_rules_by_lang(doc: spacy.tokens.Doc, lang = LanguageISO.English) -> List[str]:
-
-    result_tokens = []
-    if lang == LanguageISO.English:
-        result_tokens = _perform_tokenization_rules_for_en(doc)
-    elif lang == LanguageISO.Spanish:
-        result_tokens = _perform_tokenization_rules_for_es(doc)
-    elif lang == LanguageISO.French:
-        result_tokens = _perform_tokenization_rules_for_fr(doc)
-    elif lang == LanguageISO.Portuguese:
-        result_tokens = _perform_tokenization_rules_for_pt(doc)
-    elif lang == LanguageISO.Catalan:
-        result_tokens = _perform_tokenization_rules_for_ca(doc)
-    else: 
-        result_tokens = _perform_tokenization_rules_for_en(doc)
-
-    return result_tokens
 
 
 def get_tokens(*, text: str, lang: Optional[str] = "en") -> List[str]:
