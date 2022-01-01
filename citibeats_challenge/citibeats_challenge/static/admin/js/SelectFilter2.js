aa340004435631c84dbac9e2bcf48b0cd4cc102f
MAC <miguelangelcabrera1@hotmail.com>
2022-01-01 20:19:44 +0100

    Fix static files for production

---
+++ citibeats_challenge/citibeats_challenge/static/admin/js/SelectFilter2.js
@@ -0,0 +1,1 @@
+/Users/MAC/.virtualenvs/citibeats/lib/python3.9/site-packages/django/contrib/admin/static/admin/js/SelectFilter2.js
---
+++ citibeats_challenge/citibeats_challenge/static/admin/js/vendor/select2/select2.full.js
@@ -0,0 +1,1 @@
+/Users/MAC/.virtualenvs/citibeats/lib/python3.9/site-packages/django/contrib/admin/static/admin/js/vendor/select2/select2.full.js
