920be29e67f0feece3732424c2a1772d1c990443
MAC <miguelangelcabrera1@hotmail.com>
2022-01-01 13:39:40 +0100

    Fix typo in dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,5 @@ cd ./citibeats_challenge
 
 python manage.py collectstatic --noinput -link
 python manage.py migrate --noinput
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', 'password')" | python manage.py shell
 
 gunicorn citibeats_challenge.wsgi:application -b 0.0.0.0:8080 --access-logfile -
