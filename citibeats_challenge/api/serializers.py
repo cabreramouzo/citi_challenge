0a00fa9294a38690bb3133c1848ccff64cf56a6a
MAC <miguelangelcabrera1@hotmail.com>
2022-01-02 19:39:07 +0100

    Change Field lang to required=false

--- citibeats_challenge/api/serializers.py
+++ citibeats_challenge/api/serializers.py
@@ -1,4 +1,5 @@
 from typing import List
+from typing_extensions import Required
 
 from rest_framework import serializers
 
@@ -6,19 +7,21 @@ from common import lang
 from const import tokenizer
 
 
-
 class TextSerializer(serializers.Serializer):
     text = serializers.CharField(
-        max_length=tokenizer.MAX_CHARS_PER_TEXT, allow_blank=False
+        max_length=tokenizer.MAX_CHARS_PER_TEXT, allow_blank=False,
     )
-    lang = serializers.CharField(max_length=tokenizer.MAX_CHARS_LANG, allow_blank=True)
+    lang = serializers.CharField(max_length=tokenizer.MAX_CHARS_LANG, allow_blank=True, required=False)
 
     def validate_lang(self, value):
         value = value.lower()
-        try:
-            lang.LanguageISO(value)
-        except ValueError:
-            raise serializers.ValidationError(f"Language {value} not supported")
+        if value is None:
+            value = ""
+        if value != "":
+            try:
+                lang.LanguageISO(value)
+            except ValueError:
+                raise serializers.ValidationError(f"Language {value} not supported")
         return value
 
 
